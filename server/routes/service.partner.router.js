d281cc8ec12e66a5a47fed808898061211ccb93e
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2024-02-26 15:07:17 -0600

    refactored GET for smaller functionality

--- server/routes/service.partner.router.js
+++ server/routes/service.partner.router.js
@@ -12,47 +12,9 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 
     console.log(`Logged in User`, userId);
     const queryText = `
-    SELECT 
-    "service_partner".*, 
-    -- Behavioral history subquery
-    (SELECT coalesce(jsonb_agg(bh), '[]'::jsonb) 
-    FROM "behavioral_history" bh
-    WHERE bh."service_partner_id" = "service_partner"."service_partner_id") AS behavioral_history,
+    SELECT * FROM "service_partner" WHERE "user_id" = $1;
+    `
     
-    -- Guardianship subquery
-    (SELECT coalesce(jsonb_agg(g), '[]'::jsonb) 
-    FROM "guardianship" g
-    WHERE g."service_partner_id" = "service_partner"."service_partner_id") AS guardianship,
-    
-    -- Health Histories subquery
-    (SELECT coalesce(jsonb_agg(hh), '[]'::jsonb) 
-    FROM "health_history" hh
-    WHERE hh."service_partner_id" = "service_partner"."service_partner_id") AS health_histories,
-    
-    -- Family Histories subquery
-    (SELECT coalesce(jsonb_agg(fh), '[]'::jsonb) 
-    FROM "family_history" fh
-    WHERE fh."service_partner_id" = "service_partner"."service_partner_id") AS family_histories,
-
-    -- Consent Forms subquery
-    (SELECT coalesce(jsonb_agg(cf), '[]'::jsonb) 
-    FROM "consent_forms" cf
-    WHERE cf."service_partner_id" = "service_partner"."service_partner_id") AS consent_forms,
-    
-    -- Financial Responsibilities subquery
-    (SELECT coalesce(jsonb_agg(fr), '[]'::jsonb) 
-    FROM "financial_responsibility" fr
-    WHERE fr."service_partner_id" = "service_partner"."service_partner_id") AS financial_responsibilities,
-    
-    -- Authorizations subquery
-    (SELECT coalesce(jsonb_agg(a), '[]'::jsonb) 
-    FROM "authorizations" a
-    WHERE a."service_partner_id" = "service_partner"."service_partner_id") AS authorizations
-
-    FROM "service_partner"
-    WHERE "service_partner"."user_id" = $1  -- Filter by logged-in user's ID
-    ORDER BY "service_partner_id";
-    `;
 
     pool.query(queryText, [userId])
         .then(result => {
