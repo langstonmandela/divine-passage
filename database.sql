7a7e2468aaa55a5cad703680680d24109e413f5c
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2024-02-27 12:11:30 -0600

    updated data.sql to reflect the refactor

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
-CREATE TABLE "users" (
+CREATE TABLE "user" (
 	"id" serial NOT NULL,
 	"username" varchar(255) NOT NULL UNIQUE,
 	"password" varchar(255) NOT NULL,
 	"role" varchar(50) NOT NULL DEFAULT 'divine_employee',
 	"date_created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+	CONSTRAINT "user_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -20,21 +20,21 @@ CREATE TABLE "service_partner" (
 	"gender" varchar(50) NOT NULL DEFAULT 'not',
 	"date_of_placement" DATE NOT NULL,
 	"date_created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	"usert_id" serial NOT NULL,
+	"user_id" serial NOT NULL,
 	CONSTRAINT "service_partner_pk" PRIMARY KEY ("service_partner_id")
 ) WITH (
   OIDS=FALSE
 );
 
-CREATE TABLE "forms" (
-	"form_id" serial NOT NULL,
+CREATE TABLE "forms_aggregator" (
+	"forms_aggregator_id" serial NOT NULL,
 	"user_id" integer NOT NULL,
-	"child_id" integer NOT NULL,
-	"submission_date" TIMESTAMP NOT NULL,
+	"service_partner_id" integer NOT NULL,
+	"submission_date" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"form_type" varchar(255) NOT NULL,
 	"date_created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"status" TEXT NOT NULL,
-	CONSTRAINT "forms_pk" PRIMARY KEY ("form_id")
+	CONSTRAINT "forms_aggregator_pk" PRIMARY KEY ("forms_aggregator_id")
 ) WITH (
   OIDS=FALSE
 );
@@ -43,8 +43,8 @@ CREATE TABLE "forms" (
 CREATE TABLE "guardianship" (
 	"guardianship_id" serial NOT NULL,
 	"user_id" serial NOT NULL,
-	"child_id" serial NOT NULL,
-	"form_id" serial NOT NULL,
+	"service_partner_id" serial NOT NULL,
+	"forms_aggregator_id" serial NOT NULL,
 	"court_order_number" varchar(255) NOT NULL DEFAULT 'pending',
 	"cps_worker_name" varchar(255) NOT NULL DEFAULT 'not',
 	"cps_worker_phone" varchar(255) NOT NULL DEFAULT 'not',
@@ -54,3 +54,20 @@ CREATE TABLE "guardianship" (
 ) WITH (
   OIDS=FALSE
 );
+
+DROP TABLE "forms_aggregator";
+
+
+SELECT * FROM "service_partner" WHERE "user_id" = '1';
+
+SELECT * FROM "service_partner" WHERE "user_id" = '1' and "service_partner_id" = '6' ;
+
+UPDATE "service_partner" 
+SET "nick_name" = 'Tawny'
+WHERE "user_id" = '1' and "service_partner_id" = '6' ;
+
+INSERT INTO "forms_aggregator" ("user_id", "service_partner_id", "form_type", "status" )
+VALUES ('1', '6', 'guardianship', 'pending') 
+RETURNING "forms_aggregator_id"; 
+
+
