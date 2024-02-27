1c96eece24e8b8d0310195285033243654d6eb2f
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2024-02-27 11:37:40 -0600

    GET and POST in guardianship, post isn't quite there yet

--- server/routes/guardianship.router.js
+++ server/routes/guardianship.router.js
@@ -22,5 +22,41 @@ router.get('/', rejectUnauthenticated, (req, res) => {
             res.sendStatus(500);
         });
 });
+router.post('/', rejectUnauthenticated, (req, res) => {
+    console.log(`in POST /guardianship`);
+    const userId = req.user.id;
+    const {
+        court_order_number,
+        cps_worker_name,
+        cps_worker_phone,
+        cps_worker_email,
+        child_id,
+        form_id
+    } = req.body;
 
+    console.log(`Logged in User`, userId);
+    const queryText = `
+        INSERT INTO guardianship (
+            court_order_number,
+            cps_worker_name,
+            cps_worker_phone,
+            cps_worker_email,
+            child_id,
+            user_id,
+            form_id
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING guardianship_id;
+    `;
+    
+    pool.query(queryText, [court_order_number, cps_worker_name, cps_worker_phone, cps_worker_email, child_id, userId, form_id])
+        .then(result => {
+            const guardianshipId = result.rows[0].guardianship_id;
+            console.log(`Guardianship inserted with ID: ${guardianshipId}`);
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.error('Error in POST /guardianship', error);
+            res.sendStatus(500);
+        });
+});
 module.exports = router;
